ee716cc2b0424c706be1ae30d1de623e495b98ad
kimhyunyoung <jaykayhy@gmail.com>
2024-03-22 20:56:02 +0900

    demo 추가

---
+++ demo/markdown.js
@@ -0,0 +1,34 @@
+
+function loadShowdown(callback) {
+    let cssMarkdownCDN = document.createElement('link');
+    cssMarkdownCDN.rel = 'stylesheet';
+    cssMarkdownCDN.href = 'https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.5.1/github-markdown-light.min.css';
+    document.head.appendChild(cssMarkdownCDN);
+
+    let cssMarkdown = document.createElement('link');
+    cssMarkdown.rel = 'stylesheet';
+    cssMarkdown.href = './markdown.css';
+    document.head.appendChild(cssMarkdown);
+
+    let jsMarkdown = document.createElement('script');
+    jsMarkdown.src = 'https://cdn.jsdelivr.net/npm/showdown/dist/showdown.min.js';
+    jsMarkdown.onload = function() {
+      callback();
+    };
+    document.head.appendChild(jsMarkdown);
+}
+  
+// 로드된 후 실행할 함수
+function initializeMarkdown() {
+    let converter = new showdown.Converter();
+    let markdownContent = document.querySelectorAll('.markdown-body');
+    markdownContent.forEach(function(element) {
+        let markdown = element.innerHTML;
+        let html = converter.makeHtml(markdown);
+        element.innerHTML = html;
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    loadShowdown(initializeMarkdown);
+  });
